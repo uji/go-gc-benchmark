ccf0a69038c6580c1cb01a8e88fad12d857600a8
uji <ujiprog@gmail.com>
2022-04-16 22:36:01 +0900

    feat: benchmark.bash

---
+++ benchmark.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+_benchmark() {
+  local result
+  result=$(docker run --rm -e GOBIN=/go -e GO111MODULE=off golang:$1 /bin/bash -c "go get golang.org/x/benchmarks/garbage && ./garbage -benchtime=30s" | grep "BenchmarkGarbage")
+  echo $result >> $result.log
+}
+
+_benchmark "1.10.0"
+_benchmark "1.12.0"
+_benchmark "1.14.0"
+_benchmark "1.16.0"
+_benchmark "1.18.0"
